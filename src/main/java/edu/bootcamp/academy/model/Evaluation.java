388ec561da8175c9816e38226e59807df72c88d3
Jean Evenx Joseph <83151108+jeanevenx@users.noreply.github.com>
2023-04-09 16:41:20 -0300

    Implemented the Evaluation entity

--- src/main/java/edu/bootcamp/academy/model/Evaluation.java
+++ src/main/java/edu/bootcamp/academy/model/Evaluation.java
@@ -1,4 +1,31 @@
 package edu.bootcamp.academy.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_evaluation")
 public class Evaluation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id")
+    private Student student;
+
+    private LocalDateTime evaluationDate = LocalDateTime.now();
+
+    @Column(name = "current_weight")
+    private double weight;
+
+    @Column(name="current_height")
+    private double height;
 }
