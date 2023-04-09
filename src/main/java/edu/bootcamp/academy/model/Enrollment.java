b5f3757d25714c26ff1ae416e7cb8a8f3d46bc3c
Jean Evenx Joseph <83151108+jeanevenx@users.noreply.github.com>
2023-04-09 16:47:08 -0300

    Implemented the Enrollment entity

--- src/main/java/edu/bootcamp/academy/model/Enrollment.java
+++ src/main/java/edu/bootcamp/academy/model/Enrollment.java
@@ -1,4 +1,26 @@
 package edu.bootcamp.academy.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_enrollments")
 public class Enrollment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "student_id")
+    private Student student;
+
+    private LocalDateTime enrollmentDate = LocalDateTime.now();
 }
