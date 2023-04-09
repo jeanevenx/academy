0d8b34151815d36fbe4796b7a370f73dcd2abc54
Jean Evenx Joseph <83151108+jeanevenx@users.noreply.github.com>
2023-04-09 16:33:37 -0300

    Implemented the address entity

--- src/main/java/edu/bootcamp/academy/model/Address.java
+++ src/main/java/edu/bootcamp/academy/model/Address.java
@@ -1,7 +1,21 @@
 package edu.bootcamp.academy.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_address")
 public class Address {
 
+    @Id
     private String cep;
     private String logradouro;
     private String complemento;
