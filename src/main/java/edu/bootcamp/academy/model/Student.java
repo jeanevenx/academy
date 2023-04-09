49149a7f2a640fd32e2c587f657d6f1440db1b8a
Jean Evenx Joseph <83151108+jeanevenx@users.noreply.github.com>
2023-04-09 16:04:22 -0300

    Implemented the student entity

--- src/main/java/edu/bootcamp/academy/model/Student.java
+++ src/main/java/edu/bootcamp/academy/model/Student.java
@@ -1,20 +1,36 @@
 package edu.bootcamp.academy.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
+@Table(name = "tb_student")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     private String name;
+    @Column(name = "CPF", unique = true)
     private String cpf;
     @ManyToOne
     private Address address;
     private LocalDate yearOfBirth;
+
+    @OneToMany(mappedBy = "student", cascade = CascadeType.REMOVE , fetch = FetchType.LAZY)
+    @JsonIgnore
+    private List<Evaluation> evaluationList = new ArrayList<>();
+
 
 }
