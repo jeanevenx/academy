7979024389d90d98d0c005a3640c1c0a05f0bb7f
Jean Evenx Joseph <83151108+jeanevenx@users.noreply.github.com>
2023-04-10 04:12:46 -0300

    Implemented the address payload

--- src/main/java/edu/bootcamp/academy/model/form/AddressForm.java
+++ src/main/java/edu/bootcamp/academy/model/form/AddressForm.java
@@ -1,4 +1,13 @@
 package edu.bootcamp.academy.model.form;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AddressForm {
+    private String cep;
+    private String complemento;
 }
--- src/main/java/edu/bootcamp/academy/model/form/AddressFormUpdate.java
+++ src/main/java/edu/bootcamp/academy/model/form/AddressFormUpdate.java
@@ -1,4 +1,15 @@
 package edu.bootcamp.academy.model.form;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AddressFormUpdate {
+    private String cep;
+    private String complemento;
 }
