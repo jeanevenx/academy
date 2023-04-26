feacb14f632919dbfc4c272a0eada7e0296d21d7
Jean Evenx Joseph <83151108+jeanevenx@users.noreply.github.com>
2023-04-26 12:30:09 -0300

    Implemented a DTO

---
+++ src/main/java/edu/bootcamp/academy/DTO/StudentInfo.java
@@ -0,0 +1,12 @@
+package edu.bootcamp.academy.DTO;
+
+import lombok.Data;
+
+@Data
+public class StudentInfo {
+
+        private Integer studentId;
+        private String name;
+        private String cpf;
+
+}
